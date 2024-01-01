1ff78a5769b7d0f8ef6fb9b51b997d2a256e3021
João Sanson <joaosanson01@gmail.com>
2024-01-01 17:44:54 -0300

    creating backend structure

---
+++ backend/src/services/DeleteCustomerService.ts
@@ -0,0 +1,32 @@
+import prismaClient from '../prisma'
+
+interface DeleteCustomerProps {
+  id: string
+}
+class DeleteCustomerService {
+  async execute({ id }: DeleteCustomerProps) {
+    if (!id) {
+      throw new Error('Solicitação Inválida.')
+    }
+
+    const findCustomer = await prismaClient.customer.findFirst({
+      where: {
+        id,
+      },
+    })
+
+    if (!findCustomer) {
+      throw new Error('Usuário não existe')
+    }
+
+    await prismaClient.customer.delete({
+      where: {
+        id: findCustomer.id,
+      },
+    })
+
+    return { message: 'Deletado com sucesso!' }
+  }
+}
+
+export { DeleteCustomerService }
