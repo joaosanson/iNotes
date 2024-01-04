f9a3a105c9430734b214f8de7df9ae1dc3411448
holices <dev.holices@gmail.com>
2024-01-04 18:59:36 +0000

    deleted dependencies

--- front-end/src/pages/SignIn/index.tsx
+++ front-end/src/pages/SignIn/index.tsx
@@ -3,13 +3,9 @@ import { Link } from "react-router-dom";
 
 import { Input } from "../../components/Input";
 import { Container, Form } from "./styles";
-import { useState } from "react";
 import { Button } from "../../components/Button";
 
 export function SignIn() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-
   return (
     <Container>
       <Form>
@@ -21,14 +17,12 @@ export function SignIn() {
             placeholder="E-mail"
             type="text"
             icon={FiMail}
-            onChange={e => setEmail(e.target.value)}
           />
 
           <Input 
             placeholder="Senha"
             type="password"
             icon={FiLock}
-            onChange={e => setPassword(e.target.value)}
           />
 
           <Button title='Entrar' />
