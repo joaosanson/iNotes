ef3ba1aca40e45161100488def77dcd8a08b95e1
holices <dev.holices@gmail.com>
2024-01-05 19:04:04 +0000

    created Header component

--- front-end/src/components/Header/index.tsx
+++ front-end/src/components/Header/index.tsx
@@ -1,5 +1,24 @@
+import { Container, Logout, Profile } from "./styles";
+import { RiShutDownLine } from "react-icons/ri"
+
 export function Header() {
   return (
-    <h1>header</h1>
+    <Container>
+      <Profile>
+        <img 
+          src="https://github.com/holices.png"
+          alt="imagem do avatar"
+        />
+
+        <div>
+          <span>Bem-vindo,</span>
+          <strong>Holices Sanson</strong>
+        </div>
+      </Profile>
+
+      <Logout>
+        <RiShutDownLine />
+      </Logout>
+    </Container>
   )
 }
