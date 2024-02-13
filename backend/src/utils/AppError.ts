a7231c1927bbe99b9eb14b6de6ef5c56addd12ce
João Sanson <joaosanson01@gmail.com>
2024-02-13 17:42:19 -0300

    feat: create all routes for users

--- backend/src/utils/AppError.ts
+++ backend/src/utils/AppError.ts
@@ -10,36 +10,45 @@ export const errorHandler = (
   if (error instanceof z.ZodError) {
     const validationError = fromZodError(error)
 
-    if (validationError.name === 'ZodValidationError') {
-      reply.status(400).send(validationError.toString())
-    } else {
-      reply.status(500).send(validationError.toString())
+    switch (validationError.name) {
+      case 'ZodValidationError':
+        reply.status(400).send(validationError.toString())
+        break
+
+      default:
+        reply.status(500).send(validationError.toString())
+        break
     }
   } else {
-    if (error.message === 'User not found.') {
-      error.statusCode = 404
-      reply.status(error.statusCode).send(error)
-    }
-    if (
-      error.message ===
-      'Old password must be informed to change current password.'
-    ) {
-      error.statusCode = 400
-      reply.status(error.statusCode).send(error)
-    }
-    if (error.message === 'Password incorrect.') {
-      error.statusCode = 400
-      reply.status(error.statusCode).send(error)
-    }
-    if (error.message === 'Email already in use.') {
-      error.statusCode = 409
-      reply.status(error.statusCode).send(error)
-    }
-    if (error.message === 'One of the fields must be defined.') {
-      error.statusCode = 404
-      reply.status(error.statusCode).send(error)
-    } else {
-      reply.status(500).send(error)
+    switch (error.message) {
+      case 'User not found.':
+        error.statusCode = 404
+        reply.status(error.statusCode).send(error)
+        break
+
+      case 'Old password must be informed to change current password.':
+        error.statusCode = 400
+        reply.status(error.statusCode).send(error)
+        break
+
+      case 'Password incorrect.':
+        error.statusCode = 400
+        reply.status(error.statusCode).send(error)
+        break
+
+      case 'Email already in use.':
+        error.statusCode = 409
+        reply.status(error.statusCode).send(error)
+        break
+
+      case 'One of the fields must be defined.':
+        error.statusCode = 404
+        reply.status(error.statusCode).send(error)
+        break
+
+      default:
+        reply.status(500).send(error)
+        break
     }
   }
 }
